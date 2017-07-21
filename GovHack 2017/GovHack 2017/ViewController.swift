e5acd46dff7b3a2ae7bee95ed5b91a4e873b61d3
I0N1X <30335700+I0N1X@users.noreply.github.com>
2017-07-21 17:02:41 +1000

    Add files via upload

---
+++ GovHack 2017/GovHack 2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GovHack 2017
+//
+//  Created by Yasas Kulatunga on 7/21/17.
+//  Copyright © 2017 Yasas Kulatunga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
